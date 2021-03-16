dd4e746e5e72d55abe19380928d8d023edc964c9
Benjamin Becker <bmbecker@colgate.edu>
2021-03-16 15:24:38 -0400

    Starting to set up SQL integration. Might want to ditch the arraylist in FactionUtilities and go straight for SQL queries.

--- src/com/bmbecker/plugin/JacksaFactions.java
+++ src/com/bmbecker/plugin/JacksaFactions.java
@@ -3,9 +3,9 @@ package com.bmbecker.plugin;
 import com.bmbecker.plugin.commands.FactionCommands;
 import com.bmbecker.plugin.events.ChunkListener;
 import com.bmbecker.plugin.events.PlayerHitListener;
+import com.bmbecker.plugin.utilities.SQLUtilities;
 import com.bmbecker.plugin.utilities.WorldGuardUtilities;
 
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JacksaFactions extends JavaPlugin {
@@ -15,18 +15,20 @@ public class JacksaFactions extends JavaPlugin {
 		WorldGuardUtilities.createFlags(); // have to create FACTION_PVP flag before WorldGuard is enabled
 	}
 	
-	// Enabled method
+	// Enable method
 	@Override
 	public void onEnable() {
-		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[JacksaFactions]: Plugin enabled");
+		SQLUtilities.initConnection();
 		getCommand("faction").setExecutor(new FactionCommands());
 		getServer().getPluginManager().registerEvents(new PlayerHitListener(), this);
 		getServer().getPluginManager().registerEvents(new ChunkListener(), this);
+		getServer().getConsoleSender().sendMessage("[JacksaFactions]: Plugin enabled");
 	}
 	
 	// Disable method
 	@Override
 	public void onDisable() {
-		getServer().getConsoleSender().sendMessage(ChatColor.RED + "[JacksaFactions]: Plugin disabled");
+		SQLUtilities.closeConnection();
+		getServer().getConsoleSender().sendMessage("[JacksaFactions]: Plugin disabled");
 	}
 }
--- src/com/bmbecker/plugin/utilities/SQLUtilities.java
+++ src/com/bmbecker/plugin/utilities/SQLUtilities.java
@@ -1,5 +1,81 @@
 package com.bmbecker.plugin.utilities;
 
-public class SQLUtilities {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.bmbecker.plugin.config.Keys;
+import com.bmbecker.plugin.objects.ClaimedChunk;
+import com.bmbecker.plugin.objects.Faction;
+
+public class SQLUtilities {
+	
+	private static final String TABLE_FACTIONS = "factions";
+	private static final String TABLE_CLAIMED_CHUNKS = "claimed_chunks";
+	private static final String TABLE_PLAYERS = "players";
+	
+	private static final String SQL_ADDR = Keys.SQL_ADDR;
+	private static final String SQL_USER = Keys.SQL_USER;
+	private static final String SQL_PW = Keys.SQL_PW;
+	private static Connection con;
+	
+	
+	//TODO: Might want to load data on connection init
+	public static void initConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(SQL_ADDR, SQL_USER, SQL_PW);
+			Bukkit.getServer().getConsoleSender().sendMessage("[JacksaFactions]: Connected to SQL Database.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//TODO: Might want to save data on connection close
+	public static void closeConnection() {
+		try {
+			con.close();
+			Bukkit.getServer().getConsoleSender().sendMessage("[JacksaFactions]: SQL Connection has been closed.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void createFaction(Faction faction) {
+		try {
+			Statement stmt = con.createStatement();
+			
+			if (faction.getName().contains("\"") || faction.getName().contains(";") || faction.getName().contains("'")) { // player tried to use SQL escape keys in their faction name
+				return;
+			}
+			
+			stmt.executeUpdate("INSERT INTO " + TABLE_FACTIONS + " (name, leader) VALUES ('" + faction.getName() + "', " + faction.getLeaderUUID() + ");");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void addFactionMember(Faction faction, Player player) {
+		try {
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("UPDATE " + TABLE_PLAYERS + " SET faction='" + faction.getName() + "' WHERE player_id=" + player.getUniqueId() + ";");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void addClaimedChunk(Faction faction, ClaimedChunk chunk) {
+		try {
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("INSERT INTO " + TABLE_CLAIMED_CHUNKS + " (world, x, z, faction) VALUES ('" + chunk.getWorld() + "', " + chunk.getX() + ", " + chunk.getZ() + ", '" + faction.getName() + "');");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
